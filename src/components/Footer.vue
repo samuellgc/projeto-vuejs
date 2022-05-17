53d15b9e74e65edb8d000346e9c1790b072b96a8
Samuel <samuellgc@hotmail.com>
2022-05-17 09:21:35 -0300

    Criação do projeto e ajustes do footer e navbar

---
+++ src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+<footer id="footer">
+  <p>Make your burger &copy; 2022</p>
+</footer>
+</template>
+<script>
+export default {
+  Name: "Footer",
+};
+</script>
+
+<style scoped>
+  #footer {
+    height: 200px;
+    background-color: #222;
+    border-top: 4px solid #111;
+    color: #fcba03;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+  }
+</style>
